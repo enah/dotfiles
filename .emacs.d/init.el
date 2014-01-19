8293032b89d7dc2ec904f91fdf5468660424e716
enah <ehariyoshi@berkeley.edu>
2014-01-19 14:08:39 -0500

    changed save place and backspace mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,8 +24,10 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 (normal-erase-is-backspace-mode 0)
+(show-paren-mode 1)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(setq show-paren-delay 0)
 (setq ido-enable-flex-matching t)
 (setq uniquify-buffer-name-style 'forward)
 (setq-default save-place t)
@@ -40,28 +42,38 @@
       save-interprogram-paste-before-kill t
       apropos-do-all t
       mouse-yank-at-point t
+      backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t
       save-place-file (concat user-emacs-directory "places")
-      backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                               "backups")))))
-(custom-set-faces
+      backup-directory-alist `(("." . "~/.saves")))
+(normal-erase-is-backspace-mode 1)
+
+(set-face-attribute 'default nil
+		    :family "Monaco"
+		    :height 150)
+
+
+;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
- '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
- '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
- '(linum ((t (:foreground "black" :weight bold))))
- '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-
+; '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+; '(column-marker-1 ((t (:background "red"))))
+; '(diff-added ((t (:foreground "cyan"))))
+; '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+; '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+; '(fundamental-mode-default ((t (:inherit default))))
+; '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+; '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+; '(linum ((t (:foreground "black" :weight bold))))
+; '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+; '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+; '(show-paren-match ((((class color) (background light)) (:background "black"))))
+; '(vertical-border ((t nil))))
 
 ;; ------------
 ;; -- Macros --
